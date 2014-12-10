e59c0e6c906b9fce513c0a0a98dc3fcb86c783d2
Alvin Lao <alvin.lao.is@gmail.com>
2014-12-09 20:17:59 -0800

    basic game lobby

--- server/game.js
+++ server/game.js
@@ -1,24 +1,75 @@
+var client = require('../server/client');
+
+var NUM_PLAYERS = 1;
+var NUM_TEAMS = 1;
+
+var STATES = Object.freeze({
+	LOBBY : 1,
+	IN_GAME : 2,
+	POST_GAME: 3
+});
+
 var state = {
-	state: 'lobby',
+	state: STATES.LOBBY,
 	clients: {},
+	num_clients: 0,
 	teams: {},
 
 	connect: connect,
-	disconnect: disconnect
+	disconnect: disconnect,
+	is_ready: is_ready,
+	assign_teams: assign_teams,
+	start: start,
 }
-module.exports = state;
 
 function connect(socket) {
-	if (!(socket.id in state.clients)) {
-		state.clients[socket.id] = {
-			id: socket.id,
-			team: -1
-		}
+	if (state.num_clients >= NUM_PLAYERS) {
+		return false;
 	}
+
+	if (!(socket.id in state.clients)) {
+		state.clients[socket.id] = new client.client_object(socket.id);
+		++state.num_clients;
+	}
+
+	return true;
 }
 
 function disconnect(socket) {
 	if (socket.id in state.clients) {
 		delete state.clients[socket.id]
+		--state.num_clients;
 	}
 }
+
+function is_ready() {
+	return Object.keys(state.clients).length >= NUM_PLAYERS && state.state == STATES.LOBBY;
+}
+
+function assign_teams() {
+	var clients = Object.keys(state.clients);
+	var teams = [];
+	var team;
+
+	while (teams.length < NUM_TEAMS) {
+		team = []
+
+		while(team < NUM_PLAYERS) {
+			var index = Math.random() * clients.length;
+			team.push(clients[index])
+			clients.splice(index, 1)
+		}
+
+		teams.push(team)
+	}
+
+	return teams;
+}
+
+function start() {
+	console.log(state)
+	state.state = STATES.IN_GAME;
+}
+
+// EXPORT!
+module.exports = state;
--- server/socket.js
+++ server/socket.js
@@ -1,11 +1,29 @@
 var server = require('../server/server')
 var game = require('../server/game')
+// var brain = require('../server/brain')
 
 server.io.on('connection', function(socket) {
 	console.log('Client has connected: ' + socket.id);
 	
 	// Game connect
-	game.connect(socket);
+	if (!game.connect(socket)) {
+		console.log('Game full')
+		
+		// Game has enough people
+		socket.emit('game full', 'Game is full')
+	}
+
+	if (game.is_ready()) {
+		console.log('Start game')
+		var teams = game.assign_teams();
+
+		// Alert clients
+		server.io.emit('game start', true);
+		game.start();
+
+		// Start logic
+		// brain.start(teams);
+	}
 
 	socket.on('disconnect', function() {
 		console.log('Client disconnect')
