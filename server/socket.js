e1bc2b3f1294208a1fa1c7797ec3a700359fcec2
Alvin Lao <alvin.lao.is@gmail.com>
2014-12-09 18:38:43 -0800

    game object

--- server/socket.js
+++ server/socket.js
@@ -1,8 +1,10 @@
 var server = require('../server/server')
+var game = require('../server/game')
 
 server.io.on('connection', function(socket) {
 	console.log('Client has connected');
 	
+	server.io.emit('hello', game.state)
 	socket.on('message', function(msg) {
 		console.log(msg);
 	})
