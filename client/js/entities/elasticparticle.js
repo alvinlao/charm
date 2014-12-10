483a15461d5c5f50019698c8dcc47f637eb9e26c
Alvin Lao <alvin.lao.is@gmail.com>
2014-12-09 23:14:53 -0800

    rename hookslaw

--- client/js/entities/elasticparticle.js
+++ client/js/entities/elasticparticle.js
@@ -5,6 +5,6 @@ function ElasticParticle(x, y) {
 	Particle.prototype.constructor.call(this, x, y);
 }
 
-ElasticParticle.prototype.hooks = function(other_elastic_particle) {
+ElasticParticle.prototype.hookslaw = function(other_elastic_particle) {
 	// TODO
 }
