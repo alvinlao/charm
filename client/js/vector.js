db91babbc887ea01a3eeb392afa088f874e4980a
Alvin Lao <alvin.lao.is@gmail.com>
2014-12-10 09:44:12 -0800

    fix file linking; vector naming

--- client/js/vector.js
+++ client/js/vector.js
@@ -1,13 +1,13 @@
 function Vector2D(x, y) {
   this.x = x;
-  thix.y = y;
+  this.y = y;
   return this;
 }
 
 Vector2D.prototype.add = function(other) {
   return Vector2D(this.x + other.x, this.y + other.y);
 }
-Vector2d.prototype.scale = function(k) {
+Vector2D.prototype.scale = function(k) {
   return Vector2D(k * this.x, k * this.y);
 }
 Vector2D.prototype.subtract = function(other) {
