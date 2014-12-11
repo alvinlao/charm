c0e49a6cd8d78cf802f3c09538a12e2412d08542
Alvin Lao <alvin.lao.is@gmail.com>
2014-12-10 18:48:08 -0800

    continous input force application

--- server/brain.js
+++ server/brain.js
@@ -29,7 +29,8 @@ Brain.prototype.init_world = function() {
     this.eid = 0;
 
     // List of inputs from players
-    this.inputs = [];
+    // eid => input
+    this.inputs = {};
 }
 
 Brain.prototype.step = function() {
@@ -55,21 +56,18 @@ Brain.prototype.set_interval = function(loop, loopInterval){
 }
 
 Brain.prototype.queue_inputs = function(data) {
-    this.inputs.push(data);
+    this.inputs[data.eid] = data;
 }
 
 Brain.prototype.process_inputs = function() {
-    for (var i = 0; i < this.inputs.length; i++) {
-        var input_data = this.inputs[i];
-        var eid = input_data.eid;
+    for (var eid in this.inputs) {
+        var input_data = this.inputs[eid];
         var input = input_data.input;
 
         var force = new b2d.b2Vec2(input.x, input.y);
         force.Multiply(CONSTANTS.INPUT_MULTIPLIER);
         this.objects[eid].apply_force(force);
     }
-
-    this.inputs = [];
 }
 
 Brain.prototype.loop = function(that) {
