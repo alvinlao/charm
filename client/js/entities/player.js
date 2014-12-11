36e8e637fece19de246428ea3f93e1c64e028e2e
ggollmer <ggollmer@gmail.com>
2014-12-10 17:29:36 -0800

    Adding angle and animation to player.

--- client/js/entities/player.js
+++ client/js/entities/player.js
@@ -3,10 +3,14 @@ Player.prototype.constructor = Player;
 
 function Player(eid) {
 	Particle.prototype.constructor.call(this, eid, 100, 100);
+
+	this.dx = 0;
+	this.dy = 0;
+	this.rotation = 0;
 }
 
 Player.prototype.init_draw = function() {
-	this.drawable = canvas.display.ellipse({x: this.x, y: this.y, radius:20, fill:"white"}).add();
+	//this.drawable = canvas.display.ellipse({x: this.x, y: this.y, radius:20, fill:"white"}).add();
 	//this.drawable.strokeColor = "white";
 
 	this.trail = canvas.display.image({
@@ -32,44 +36,85 @@ Player.prototype.emitState = function(socket) {
 Player.prototype.control = function(state) {
 	//Particle.prototype.update.call(this);
 	//TODO; Change to force
-	var x = 0;
-	var y = 0;
+	var odx = this.dx;
+	var ody = this.dy;
+	this.dx = 0;
+	this.dy = 0;
+
 	if(controls.isControlDown(controls.key_map.up)) {
-		y += 1;
+		this.dy -= 1;
 	}
 	if(controls.isControlDown(controls.key_map.down)) {
-		y -= 1;
+		this.dy += 1;
 	}
 	if(controls.isControlDown(controls.key_map.left)) {
-		x -= 1;
+		this.dx -= 1;
 	}
 	if(controls.isControlDown(controls.key_map.right)) {
-		x += 1;
+		this.dx += 1;
 	}
-	if(x != 0 && y != 0) {
-		x *= 0.7071;
-		y *= 0.7071;
+	if(this.dx != 0 && this.dy != 0) {
+		this.dx *= 0.7071;
+		this.dy *= 0.7071;
 	}
 
-	console.log(JSON.stringify({eid:this.eid, input:{x:x, y:y}}));
+	this.x += this.dx;
+	this.y += this.dy;
 
-    socket.emit("inputs", {eid:this.eid, input:{x:x, y:y}});
+	if(this.dx != 0 || this.dy != 0) {
+		this.rotation = toDegrees(Math.atan2(this.dy, this.dx)) + 90;
+	}
+
+	if(this.dx != odx || this.dy != ody) {
+		console.log(JSON.stringify({eid:this.eid, input:{x:this.dx, y:this.dy}}));
+
+	    //socket.emit("inputs", {eid:this.eid, input:{x:this.dx, y:this.dy}});
+    }
 }
 
 Player.prototype.replicate = function(state) {
-	//this.drawable.strokeWidth = 3.0 - 2.0*Math.sqrt(Math.pow(state.x - this.x, 2) + Math.pow(state.y - this.y,2))/8.5;	
+	this.dx = state.x - this.x; this.dx /= this.dx;
+	this.dy = state.y - this.y;	this.dx /= this.dx;
+
+	if(this.dx != 0 && this.dy != 0) {
+		this.dx *= 0.7071;
+		this.dy *= 0.7071;
+	}
+
+	if(this.dx != 0 || this.dy != 0) {
+		this.rotation = toDegrees(Math.atan2(this.dy, this.dx)) + 90;
+	}
 
 	this.x = state.x;
 	this.y = state.y;
 
-	//this.drawable.fill = "#FFAAAA";
 }
 
 Player.prototype.draw = function() {
 	Particle.prototype.draw.call(this);
+	this.drawable.rotateTo( this.rotation );
 	this.drawable.moveTo(this.x, this.y);
+
+	if(this.dx != 0 || this.dy != 0) {
+		this.trail.opacity = 1;
+		this.trail.rotateTo( this.rotation );
+
+		var trail_offset = -24 + (4 - Math.random()*8);
+		this.trail.moveTo(this.x + Math.cos(toRadians(this.rotation - 90))*trail_offset, this.y + Math.sin(toRadians(this.rotation - 90))*trail_offset);
+	} else {
+		this.trail.opacity = 0;
+	}
 }
 
 Player.prototype.destroy = function() {
 	this.drawable.remove();
+}
+
+
+function toDegrees(rad) {
+	return rad * 180/Math.PI;
+}
+
+function toRadians(deg) {
+	return deg * Math.PI/180;
 }
