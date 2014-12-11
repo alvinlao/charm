4ae8c408f7e1db9829883176c3fbad1298999f65
Bai Li <luckytoilet@gmail.com>
2014-12-11 14:55:48 -0800

    decrease initial asteroid velocity

--- server/models/entities/asteroid.js
+++ server/models/entities/asteroid.js
@@ -14,7 +14,7 @@ function Asteroid(world, eid, x, y, r, active) {
         friction: CONSTANTS.ASTEROID_FRICTION,
     };
 	Particle.prototype.constructor.call(this, world, eid, x, y, CONSTANTS.ASTEROID_MASS, r, params, CONSTANTS.TYPE_ASTEROID);
-    var velocity_vector = new b2d.b2Vec2(Math.random()*10, Math.random()*10);
+    var velocity_vector = new b2d.b2Vec2(Math.random()*2-1, Math.random()*2-1);
     this.body.SetLinearVelocity(velocity_vector);
     this.r = r;
     return this;
